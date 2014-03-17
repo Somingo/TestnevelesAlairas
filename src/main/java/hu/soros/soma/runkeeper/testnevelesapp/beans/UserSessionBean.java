6fcc6590644640169890d7559fe8b86bdcbf1e7f
somingo <somingo@SECRET-NO1>
2014-03-18 00:12:08 +0100

    POM - JSF dependency Added added dummy sessionbean added CDI beans.xml, faces-config.xml web.xml added index.xhtml dummy

---
+++ src/main/java/hu/soros/soma/runkeeper/testnevelesapp/beans/UserSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hu.soros.soma.runkeeper.testnevelesapp.beans;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author somingo
+ */
+@Named
+@SessionScoped
+public class UserSessionBean implements Serializable{
+    
+}
